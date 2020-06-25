1083f4108726a4cb049c1150f76f8b50635c7658
Mark <Mark@DESKTOP-V1BKCDT>
2020-06-25 22:13:10 +1200

    add config and c++17 test

--- test/core/Mat4x4Tests.cpp
+++ test/core/Mat4x4Tests.cpp
@@ -5,4 +5,13 @@ TEST_CASE("Testing mat4x4 1", "[Mat4x4]") {
 	ze::Mat4x4 mat(1.0f);
 
 	REQUIRE(1.0f == mat[0]);
+
+	auto fxn = []() -> int { return 11; };
+	
+	if (auto v = fxn(); v == 11) {
+		REQUIRE(true);
+	}
+	else {
+		REQUIRE(false);
+	}
 }
