eb1ebf68694d379cd751165b416f91a704bd8acb
Mark <Mark@DESKTOP-V1BKCDT>
2020-06-25 23:53:03 +1200

    win build 18

--- scripts/windows_build.ps1
+++ scripts/windows_build.ps1
@@ -7,7 +7,7 @@ Write-Host "===================================" -ForegroundColor Yellow
 Write-Host "Generating Build Files" -ForegroundColor Yellow
 Write-Host "===================================" -ForegroundColor Yellow
 
-$cmd = 'cmake -G "Visual Studio 15 2017" --config ' + $build_type + ' ..'
+$cmd = 'cmake -DCMAKE_BUILD_TYPE=' + $build_type + ' ..'
 Write-Host $cmd -ForegroundColor Cyan
 Invoke-Expression $cmd
 
@@ -19,19 +19,19 @@ $cmd = 'cmake --build . --config ' + $build_type
 Write-Host $cmd -ForegroundColor Cyan
 Invoke-Expression $cmd
 
-Write-Host "===================================" -ForegroundColor Yellow
-Write-Host "Installing Artifacts" -ForegroundColor Yellow
-Write-Host "===================================" -ForegroundColor Yellow
-
-New-Item -Path $intall_dir_root -ItemType directory | Out-Null
-$cmd = 'cmake --install . --prefix installed --config ' + $build_type
-Write-Host $cmd -ForegroundColor Cyan
-Invoke-Expression $cmd
-
 #Write-Host "===================================" -ForegroundColor Yellow
-#Write-Host "Running Tests" -ForegroundColor Yellow
+#Write-Host "Installing Artifacts" -ForegroundColor Yellow
 #Write-Host "===================================" -ForegroundColor Yellow
-
-#$cmd = 'ctest --build-config ' + $build_type + ' --output-on-failure --no-label-summary --no-subproject-summary --progress'
+#
+#New-Item -Path $intall_dir_root -ItemType directory | Out-Null
+#$cmd = 'cmake --install . --prefix installed --config ' + $build_type
 #Write-Host $cmd -ForegroundColor Cyan
 #Invoke-Expression $cmd
+
+Write-Host "===================================" -ForegroundColor Yellow
+Write-Host "Running Tests" -ForegroundColor Yellow
+Write-Host "===================================" -ForegroundColor Yellow
+
+$cmd = 'ctest --build-config ' + $build_type + ' --output-on-failure --no-label-summary --no-subproject-summary --progress'
+Write-Host $cmd -ForegroundColor Cyan
+Invoke-Expression $cmd
