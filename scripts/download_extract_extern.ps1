8cc89adcbe5ab29809da79670762274d671e5fb4
Mark <Mark@DESKTOP-V1BKCDT>
2020-07-04 03:59:27 +1200

    Remove logging, try older version

--- scripts/download_extract_extern.ps1
+++ scripts/download_extract_extern.ps1
@@ -4,7 +4,7 @@ mkdir external
 
 cd external
 
-wget https://kcat.strangesoft.net/openal-binaries/openal-soft-1.20.1-bin.zip -O OpenAL.zip
+wget https://kcat.strangesoft.net/openal-binaries/openal-soft-1.19.1-bin.zip -O OpenAL.zip
 
 7z x OpenAL.zip -oOpenAL
 
