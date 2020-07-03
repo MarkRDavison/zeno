ad49ae0b95974982f9324d980ec07edda4ac294e
Mark <Mark@DESKTOP-V1BKCDT>
2020-07-04 03:37:48 +1200

    this time

--- scripts/download_extract_extern.ps1
+++ scripts/download_extract_extern.ps1
@@ -1,5 +1,3 @@
-pwd
-
 cd ..
 
 mkdir external
@@ -10,24 +8,6 @@ wget https://kcat.strangesoft.net/openal-binaries/openal-soft-1.20.1-bin.zip -O 
 
 7z x OpenAL.zip -oOpenAL
 
-pwd
-
-cd OpenAL
-
-pwd
-
 cd ..
-
-pwd
-
-cd ..
-
-pwd
 
 cd zeno
-
-pwd
-
-ls C:/Users/travis/build/MarkRDavison/external/OpenAL
-ls C:/Users/travis/build/MarkRDavison/external/OpenAL/include
-ls C:/Users/travis/build/MarkRDavison/external/OpenAL/include/AL
